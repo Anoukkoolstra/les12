b8b9f84a51f56d8330e6fb0a05d6bd1945be9254
Anoukkoolstra <115786628+Anoukkoolstra@users.noreply.github.com>
2023-04-06 22:33:17 +0200

    Add files via upload

---
+++ script12-1.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#resultaatKnop').on('click', function(){
+    });
+
+    $(window).on('load', function() {
+        alert('De hele HTML-pagina is ingeladen!');
+      });
+});
