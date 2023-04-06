c3413c8d51f616d1572cba978d93487df15fcedc
Anoukkoolstra <115786628+Anoukkoolstra@users.noreply.github.com>
2023-04-06 22:33:04 +0200

    Delete script12-1.js

--- script12-1.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#resultaatKnop').on('click', function(){
-        $('#loadResultaat').load('content12.html', function() {
-            alert('Test');
-        });
-    });
-});
